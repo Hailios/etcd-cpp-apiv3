4437d1688c17746c2d201af901a2b8300b196663
Jakob Lovhall <lovhall@protonmail.com>
2026-04-03 15:46:38 +0200

    make it build with boost 1.89

--- etcd-cpp-api-config.in.cmake
+++ etcd-cpp-api-config.in.cmake
@@ -21,13 +21,33 @@ if(cpprestsdk_FOUND)
 endif()
 
 set(ETCD_CPP_HOME "${CMAKE_CURRENT_LIST_DIR}/../../..")
-include("${CMAKE_CURRENT_LIST_DIR}/etcd-targets.cmake")
+#include("${CMAKE_CURRENT_LIST_DIR}/etcd-targets.cmake")
+
+message(STATUS "Looking for etcd-targets.cmake in: ${CMAKE_CURRENT_LIST_DIR}")
+#file(GLOB TARGETS_FILE "${CMAKE_CURRENT_LIST_DIR}/cpprestsdk-targets.cmake")
+#if(TARGETS_FILE)
+#  message(STATUS "Found cpprestsdk-targets.cmake at: ${TARGETS_FILE}")
+#  include("${TARGETS_FILE}")
+#else()
+#  message(FATAL_ERROR "cpprestsdk-targets.cmake not found in ${CMAKE_CURRENT_LIST_DIR}")
+#endif()
+
+#set(TARGETS_FILE "${CMAKE_CURRENT_LIST_DIR}/../../CMakeFiles/Export/0f425fe7e2467babc1de94b92f87002d/cpprestsdk-targets.cmake")
+set(TARGETS_FILE "${CMAKE_CURRENT_LIST_DIR}/CMakeFiles/Export/09a1cce3d3c3326e68846f694d0a26ca/etcd-targets.cmake")
+if(EXISTS "${TARGETS_FILE}")
+    message(STATUS "Found targets file at: ${TARGETS_FILE}")
+    include("${TARGETS_FILE}")
+else()
+    message(FATAL_ERROR "Targets file NOT found at: ${TARGETS_FILE}")
+endif()
+
 
 set(etcd-cpp-api_FOUND TRUE)
 set(ETCD_CPP_LIBRARIES etcd-cpp-api)
 set(ETCD_CPP_CORE_LIBRARIES etcd-cpp-api-core)
 set(ETCD_CPP_INCLUDE_DIR "${ETCD_CPP_HOME}/include")
 set(ETCD_CPP_INCLUDE_DIRS "${ETCD_CPP_INCLUDE_DIR}")
+message("ffff ETCD_CPP_INCLUDE_DIR: ${ETCD_CPP_INCLUDE_DIR}")
 
 include(FindPackageMessage)
 find_package_message(etcd
